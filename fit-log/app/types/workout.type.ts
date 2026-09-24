07c48d3422f9a48a6c5b904fccd046c65613154e
Raju204116 <raju204116@gmail.com>
2026-09-24 12:28:32 +0600

    project-structure-added

---
+++ fit-log/app/types/workout.type.ts
@@ -0,0 +1,15 @@
+export type WorkoutType = {
+  id: number;
+  name: string;
+  image: string;
+  muscleGroups: string[];
+  equipment: string;
+  difficulty: string;
+  duration: number;
+  caloriesBurned: number;
+  sets: number;
+  reps: string;
+  rating: number;
+  description: string;
+  instructions: string[];
+};
