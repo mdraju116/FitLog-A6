939db41b86d5a2ac1091c0e4a4980832a8249bb3
Raju204116 <raju204116@gmail.com>
2026-09-24 18:37:54 +0600

    workout-library-completed

--- fit-log/app/components/homepage/WorkoutLibrary.tsx
+++ fit-log/app/components/homepage/WorkoutLibrary.tsx
@@ -1,22 +1,49 @@
 import WorkoutLibraryCard from "../shared/WorkoutLibraryCard";
+import { WorkoutType } from "@/app/types/workoutType";
 
 
-const WorkoutLibrary = () => {
+const getWorkouts = async ():Promise<WorkoutType[]> => {
+    try {
+        const response = await fetch('https://api.abcz.workers.dev/api/fitlog');
+        const workouts = await response.json();
+        return workouts;
+    } catch (error) {
+        console.log("Found error fetching data :", error);
+        return [];
+    }
+}
+
+
+const WorkoutLibrary = async () => {
+
+    const workouts = await getWorkouts();
+
+    // console.log("workouts:",workouts);
+
     return (
         <div className="container mx-auto px-26 space-y-2">
             <h1 className="text-white text-4xl font-bold">THE LIBRARY</h1>
-            <p className="text-[14px] font-normal text-[#9ca3af]">Twelve lifts covering every major muscle group.</p>
-        
-        <div className="grid grid-cols-3 gap-2">
-            
-            <WorkoutLibraryCard></WorkoutLibraryCard>
-        
-        </div>
+            <p className="text-[14px] font-normal text-[#9ca3af] mb-8">Twelve lifts covering every major muscle group.</p>
+
+            <div className="grid grid-cols-3 gap-3 space-y-2 ">
+
+
+            {
+                
+             workouts.map((workout)=><WorkoutLibraryCard key={workout.id} workout={workout}/>)
+                
+            }
+
+                
+
+                
+
+            </div>
 
 
         </div>
 
-        
+
     );
 };
 
--- fit-log/app/components/shared/WorkoutLibraryCard.tsx
+++ fit-log/app/components/shared/WorkoutLibraryCard.tsx
@@ -1,11 +1,73 @@
+import Image from "next/image";
+import type { WorkoutType } from "@/app/types/workoutType";
+import { GoFlame } from "react-icons/go";
+import { IoTimeOutline } from "react-icons/io5";
+import { MdOutlineStarRate } from "react-icons/md";
 
+type WorkoutCardProps = {
+    workout: WorkoutType;
+};
 
-const WorkoutLibraryCard = () => {
+const WorkoutCard = ({ workout }: WorkoutCardProps) => {
     return (
-        <div>
-            This is card
+        <div className="bg-[#111318] rounded-xl overflow-hidden shadow-md">
+
+            {/* Image */}
+            <Image
+                src={workout.image}
+                alt={workout.name}
+                width={500}
+                height={300}
+                className="w-full h-56 object-fill"
+            />
+
+            {/* Content */}
+            <div className="p-5">
+
+                {/* Muscle Groups */}
+                <div className="flex flex-wrap gap-2 mb-3">
+                    {workout.muscleGroups.map((muscle) => (
+                        <span
+                            key={muscle}
+                            className="badge badge-sm bg-[#c2f800] text-black border-none font-bold text-[12px]"
+                        >
+                            {muscle}
+                        </span>
+                    ))}
+                </div>
+
+                {/* Name */}
+                <h2 className="text-xl font-bold text-white">
+                    {workout.name}
+                </h2>
+
+                {/* Equipment */}
+                <p className="text-sm text-gray-400 mt-1">
+                    {workout.equipment}
+                </p>
+
+                {/* Stats */}
+                <div className="flex flex-wrap gap-2 mt-5">
+
+                    {/* Duration */}
+                    <span className="flex items-center gap-2 px-3 py-2 rounded-lg shadow bg-[#1b1e24] text-sm text-gray-300">
+                        <IoTimeOutline /> {workout.duration} min
+                    </span>
+
+                    {/* Calories */}
+                    <span className="flex items-center gap-2 px-3 py-2 rounded-lg shadow bg-[#1b1e24] text-sm text-gray-300">
+                        <GoFlame /> {workout.caloriesBurned} kcal
+                    </span>
+
+                    {/* Rating */}
+                    <span className="flex items-center gap-2 px-3 py-2 rounded-lg shadow bg-[#1b1e24] text-sm text-gray-300">
+                        <MdOutlineStarRate /> {workout.rating}
+                    </span>
+
+                </div>
+            </div>
         </div>
     );
 };
 
-export default WorkoutLibraryCard;
+export default WorkoutCard;
